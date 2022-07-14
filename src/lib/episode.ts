ff709dda9f8e3db7f4d7d415a437b35d327f9a43
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-14 06:51:26 -0400

    feat: Add episode cmd using episode manager

---
+++ src/lib/episode.ts
@@ -0,0 +1,34 @@
+import type { Program } from ".";
+
+import { EpisodeManager } from "../manager/EpisodeManager";
+
+const manager = new EpisodeManager();
+
+export default { execute: (program: Program) => {
+    program
+        .option("--ep --episode <name>","search for a particular episode")
+        .action((options: any) => {
+            manager.fetchAll({filter: {name: options.episode}})
+                .then(result => {
+                    result = options.all ?
+                            result :
+                            result.slice(0, 10);
+                            // original result : limit 10
+                    result
+                        .forEach( ep => {
+                            console.log(`
+${ep.name}
+air_date: ${ep.airDate}
+episode: ${ep.episode}
+characters: ${ep.characters}
+url: ${ep.url}
+created: ${ep.airTimestamp}`)
+
+                        })
+                })
+                .catch(() => {
+                    console.log("Episode not found in our archive.")
+                })
+        })
+    }
+}
