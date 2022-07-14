5ca283cc91c6b6f87386c49289f7f1c09075a8c5
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-13 20:21:18 -0400

    feat: Implement lib into commander

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { createCommand } from "commander";
 
+// cli commands
+import setup from "./lib";
+
 const program = createCommand();
 
 program
     .name('rickchurian')
     .usage('[year]')
 
+setup(program); // lib/index.ts adding additional commands
 program.parse()
